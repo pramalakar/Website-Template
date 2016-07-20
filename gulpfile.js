8fabd79816caf519268bbbbe5ea34cc6996dd0cd
Prakash Malakar <prakashm@silversystems.com.au>
2016-07-20 23:48:19 +1000

    Gulp project setup

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  webserver = require('gulp-webserver'),
+  less = require('gulp-less')
+  DEST = 'dist';
+gulp.task('js', function() {
+  gulp.src('builds/development/js/**/*')
+});
+
+gulp.task('html', function() {
+  gulp.src('builds/development/*.html')
+});
+
+gulp.task('css', function() {
+  gulp.src('builds/development/css/*.css')
+});
+gulp.task('less', function() {
+  gulp.src('builds/development/less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('builds/development/css'))
+});
+gulp.task('watch', function() {
+  gulp.watch('builds/development/js/**/*', ['js']);
+  gulp.watch('builds/development/css/*.css', ['css']);
+  gulp.watch('builds/development/less/*.less', ['less']);
+  gulp.watch(['builds/development/*.html',
+    'builds/development/views/*.html'], ['html']);
+});
+
+gulp.task('webserver', function() {
+  gulp.src('builds/development/')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('default', ['watch', 'html', 'js', 'css', 'less', 'webserver']);
